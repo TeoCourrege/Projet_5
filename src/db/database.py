9b10bc0fa794a057e9d8d2413710fa4e452cd35c
TeoPolytech <teo.courrege@etu.univ-cotedazur.fr>
2026-04-13 12:54:41 +0200

    [fix] enleve ayant enfant tests dans la database

--- src/db/database.py
+++ src/db/database.py
@@ -67,7 +67,6 @@ class ModelInput(Base):
     distance_domicile_travail = Column(Integer)
     niveau_education = Column(Integer)
     domaine_etude = Column(String(100))
-    ayant_enfants = Column(Boolean)
     frequence_deplacement = Column(String(50))
     annees_depuis_la_derniere_promotion = Column(Integer)
     annes_sous_responsable_actuel = Column(Integer)
@@ -168,7 +167,7 @@ FEATURE_COLUMNS = [
     "annees_dans_le_poste_actuel",
     "nombre_participation_pee", "nb_formations_suivies",
     "nombre_employee_sous_responsabilite", "distance_domicile_travail",
-    "niveau_education", "domaine_etude", "ayant_enfants",
+    "niveau_education", "domaine_etude",
     "frequence_deplacement", "annees_depuis_la_derniere_promotion",
     "annes_sous_responsable_actuel", "satisfaction_employee_environnement",
     "note_evaluation_precedente", "niveau_hierarchique_poste",
@@ -186,7 +185,7 @@ def predict(
     nombre_experiences_precedentes, nombre_heures_travailless, annee_experience_totale,
     annees_dans_l_entreprise, annees_dans_le_poste_actuel,
     nombre_participation_pee, nb_formations_suivies, nombre_employee_sous_responsabilite,
-    distance_domicile_travail, niveau_education, domaine_etude, ayant_enfants,
+    distance_domicile_travail, niveau_education, domaine_etude,
     frequence_deplacement, annees_depuis_la_derniere_promotion,
     annes_sous_responsable_actuel, satisfaction_employee_environnement,
     note_evaluation_precedente, niveau_hierarchique_poste,
@@ -210,7 +209,6 @@ def predict(
             nombre_employee_sous_responsabilite=nombre_employee_sous_responsabilite,
             distance_domicile_travail=distance_domicile_travail,
             niveau_education=niveau_education, domaine_etude=domaine_etude,
-            ayant_enfants=ayant_enfants in ["Oui", True, "Y"],
             frequence_deplacement=frequence_deplacement,
             annees_depuis_la_derniere_promotion=annees_depuis_la_derniere_promotion,
             annes_sous_responsable_actuel=annes_sous_responsable_actuel,
@@ -290,7 +288,6 @@ def batch_predict(file):
                 distance_domicile_travail=row.get("distance_domicile_travail"),
                 niveau_education=row.get("niveau_education"),
                 domaine_etude=row.get("domaine_etude"),
-                ayant_enfants=row.get("ayant_enfants"),
                 frequence_deplacement=row.get("frequence_deplacement"),
                 annees_depuis_la_derniere_promotion=row.get("annees_depuis_la_derniere_promotion"),
                 annes_sous_responsable_actuel=row.get("annes_sous_responsable_actuel"),
