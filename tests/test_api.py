c2d0f6e9a5e37e96f63987860036ff3df8893fb2
TeoPolytech <teo.courrege@etu.univ-cotedazur.fr>
2026-04-14 14:34:38 +0200

    [add] test predict

--- tests/test_api.py
+++ tests/test_api.py
@@ -86,14 +86,14 @@ def test_predict_success():
 
         result = predict(
             employee_id=1, age=30, genre="M", revenu_mensuel=3000,
-            statut_marital="Célibataire", departement="IT", poste="Dev",
+            statut_marital="Célibataire", departement="Commercial", poste="Cadre Commercial",
             nombre_experiences_precedentes=2, nombre_heures_travailless=40,
             annee_experience_totale=5, annees_dans_l_entreprise=2,
             annees_dans_le_poste_actuel=1,
             nombre_participation_pee=0, nb_formations_suivies=1,
             nombre_employee_sous_responsabilite=0,
             distance_domicile_travail=10, niveau_education=3,
-            domaine_etude="Info", frequence_deplacement="Rare",
+            domaine_etude="Infra & Cloud", frequence_deplacement="Occasionnel",
             annees_depuis_la_derniere_promotion=1,
             annes_sous_responsable_actuel=0,
             satisfaction_employee_environnement=3,
@@ -108,3 +108,38 @@ def test_predict_success():
         )
 
         assert result == 1
+
+
+def test_predict_error():
+    from src.db.database import predict
+
+    with patch("src.db.database._get_pipeline", side_effect=Exception("fail")), \
+         patch("src.db.database.SessionLocal") as mock_session:
+
+        session = MagicMock()
+        mock_session.return_value = session
+
+        result = predict(
+            employee_id=1, age=30, genre="M", revenu_mensuel=3000,
+            statut_marital="Célibataire", departement="Commercial", poste="Cadre Commercial",
+            nombre_experiences_precedentes=2, nombre_heures_travailless=40,
+            annee_experience_totale=5, annees_dans_l_entreprise=2,
+            annees_dans_le_poste_actuel=1,
+            nombre_participation_pee=0, nb_formations_suivies=1,
+            nombre_employee_sous_responsabilite=0,
+            distance_domicile_travail=10, niveau_education=3,
+            domaine_etude="Infra & Cloud", frequence_deplacement="Occasionnel",
+            annees_depuis_la_derniere_promotion=1,
+            annes_sous_responsable_actuel=0,
+            satisfaction_employee_environnement=3,
+            note_evaluation_precedente=3,
+            niveau_hierarchique_poste=2,
+            satisfaction_employee_nature_travail=3,
+            satisfaction_employee_equipe=3,
+            satisfaction_employee_equilibre_pro_perso=3,
+            note_evaluation_actuelle=3,
+            heure_supplementaires="Non",
+            augementation_salaire_precedente=5.0)
+
+        assert "Erreur" in result
+        session.rollback.assert_called()
